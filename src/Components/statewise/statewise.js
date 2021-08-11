6368ce801519c637d0cd19d612817af3a3b366cd
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-08-11 11:15:24 +0530

    Api called and structred in table

--- src/Components/statewise/statewise.js
+++ src/Components/statewise/statewise.js
@@ -1,14 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 
-const Statewise = () =>{
+const Statewise = () => {
+    const [data, setData] = useState([]);
+    const getCovidData = async () => {
+        const res = await fetch('https://data.covid19india.org/data.json');
+        const actualData = await res.json();
+        console.log(actualData.statewise)
+        setData(actualData.statewise);
+    }
 
-return (
-    <>
-    <h1>India Covid-19 DashBoard</h1>
-    </>
 
-)
+    useEffect(() => {
+        getCovidData();
+    }, [])
+    return (
+        <>
+            <div className='container-fluid mt-5'>
+                <div className='main-heading'>
+                    <h1 className='mb-5 text-center'> <span className='font-weight-bold'> INDIA </span>Covid-19 DashBoard</h1>
+                </div>
+                <div className='table-responsive'>
+                    <table className='table table-hover'>
+
+                        <thead className="thead-dark">
+                            <tr>
+                                <td> State </td>
+                                <td> Confirmed </td>
+                                <td> recovered </td>
+                                <td> deaths </td>
+                                <td> active </td>
+                                <td> updated </td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                data.map((curElem, ind) => {
+                                    return (
+                                        <tr key={ind}>
+                                            <th> {curElem.state}</th>
+                                            <td> {curElem.confirmed} </td>
+                                            <td> {curElem.recovered} </td>
+                                            <td> {curElem.deaths} </td>
+                                            <td> {curElem.active} </td>
+                                            <td> {curElem.lastupdatedtime} </td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </>
+
+    )
 
 
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './Components/statewise/statewise.css'
+import '../node_modules/bootstrap/dist/js/bootstrap'
 
 
 ReactDOM.render(
