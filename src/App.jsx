2be3e5fc0f0c37db3e8ea02afa1b510a22619e2b
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-08-21 01:03:49 +0530

    Covid 19 DashBoard -Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Statewise from './Components/statewise/statewise';
+
+
+
+
+const App = () =>{
+    return (
+        <>
+        <Statewise />
+        </>
+    )
+}
+
+
+export default App;
---
+++ src/Components/statewise/statewise.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Statewise = () =>{
+
+return (
+    <>
+    <h1>India Covid-19 DashBoard</h1>
+    </>
+
+)
+
+
+
+}
+
+
+
+export default Statewise;
